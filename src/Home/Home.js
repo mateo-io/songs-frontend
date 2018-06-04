8b175286e009da29783df7ea06060aeab4311f21
Mateo Mejia <mateomgj@gmail.com>
2018-06-04 11:52:57 -0500

    [release] minimum viable product
    Includes translated lyrics fetched from server.

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -4,6 +4,7 @@ import YouTube from 'react-youtube'
 
 import Paper from 'material-ui/Paper'
 import Grid from 'material-ui/Grid'
+import Button from 'material-ui/Button'
 
 // import { Observable } from 'rxjs'
 import { Subject } from 'rxjs/Subject'
@@ -23,7 +24,10 @@ export default class Home extends React.Component {
       name: '',
       results: [],
       selectedSong: 0,
-      lyrics: 'Song lyrics...if you choose one',
+      error: false,
+      loading: false,
+      lyrics: '',
+      translatedLyrics: ''
     }
   }
 
@@ -37,7 +41,7 @@ export default class Home extends React.Component {
     inputStream.subscribe(query => {
       console.log(`input subscribibtion value ${query}`)
       // sets results to an empty array as input changed
-      this.setState({ name: query, results: [] }, () =>
+      this.setState({ name: query, results: [], lyrics: '', translatedLyrics: '' }, () =>
         this.getSearchResults(this.state.name)
       )
     })
@@ -51,11 +55,7 @@ export default class Home extends React.Component {
     })
   }
 
-  getSearchContents(song) {
-    console.log(`getSearchContents with song ${JSON.stringify(song)}`)
-    this.setState({
-      selectedSong: song.videoId,
-    })
+  scrapeLyrics(song, songId, language) {
     const query = `${song.artist} ${song.match}`
     fetch('http://localhost:8000/api/scrape', {
       method: 'POST',
@@ -64,20 +64,129 @@ export default class Home extends React.Component {
       }),
       body: JSON.stringify({
         query,
-        language: 'english',
+        language,
       }),
     })
       .then(res => res.json())
       .then(data => {
         console.log(`getLyrics answered with ${data} - ${JSON.stringify(data)}`)
-        this.setState({ lyrics: data.payload })
+        const { lyrics, fetchedFrom } = data
+        // TODO make request to save the lyrics
+        this.saveLyrics({
+          songId,
+          language,
+          fetchedFrom,
+          content: lyrics,
+        })
+        this.setState({ lyrics: data.lyrics, loading: false, error: false })
       })
-      .catch(err => console.error(`Error fetching lyrics`))
+      .catch(err => {
+        console.error(`Error scraping lyrics`)
+        this.setState({ error: true })
+      })
+  }
+
+  // content, language, fetchedFrom, songId
+  saveLyrics({ songId, language, fetchedFrom, content }) {
+    console.warn(
+      `save lyrics with ${songId} ${language} ${fetchedFrom} ${content}`
+    )
+    return fetch('http://localhost:8000/api/lyrics', {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      }),
+      body: JSON.stringify({
+        songId,
+        fetchedFrom,
+        language,
+        content,
+      }),
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(
+          `saveLyrics answered with ${data} - ${JSON.stringify(data)}`
+        )
+        // if lyrics are found then set lyrics
+      })
+      .catch(err => {
+        this.setState({ error: true })
+        console.error(`Error saving lyrics`)
+      })
+  }
+
+  fetchLyrics(song, language) {
+    const query = `${song.artist} ${song.match}`
+    return fetch('http://localhost:8000/api/lyrics-fetch', {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      }),
+      body: JSON.stringify({
+        artistName: song.artist,
+        songTitle: song.match,
+        language,
+      }),
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(
+          `fetchLyrics answered with ${data} - ${JSON.stringify(data)}`
+        )
+        // if lyrics are found then set lyrics
+        if (data.found) {
+          console.log(`Lyrics exist for song ${song.match}`)
+          this.setState({ lyrics: data.lyrics.content })
+        } else {
+          // TODO -> why song and song.id ?
+          this.setState({ loading: true })
+          this.scrapeLyrics(song, data.song.id, language)
+        }
+      })
+      .catch(err => {
+        this.setState({ error: true })
+        console.error(`Error fetching lyrics`)
+      })
+  }
+
+  getSearchContents(song) {
+    console.log(`getSearchContents with song ${JSON.stringify(song)}`)
+    this.setState(
+      {
+        selectedSong: song.videoId,
+      },
+      () => {
+        // todo -> get song language
+        this.fetchLyrics(song, 'english')
+      }
+    )
+  }
+
+  translateText(targetLanguage) {
+    fetch(`http://localhost:8000/api/translate`, {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      }),
+      body: JSON.stringify({
+        text: this.state.lyrics,
+        target: targetLanguage
+      }),
+    })
+    .then(responseToParse => responseToParse.text())
+    .then(res => {
+      console.log(`translated text ${JSON.stringify(res)}`)
+      this.setState({ translatedLyrics: res })
+    })
+    .catch(err => {
+      console.error(`translate text error ${err}`)
+    })
   }
 
   render() {
     // console.log(translateText('i love you', 'en', ''))
-    const { results, selectedSong } = this.state
+    const { results, selectedSong, loading, translatedLyrics } = this.state
     console.log(`selectedSong ${selectedSong}`)
     return (
       <Root>
@@ -112,13 +221,31 @@ export default class Home extends React.Component {
 
           <Grid item md={6} xs={12}>
             <WhitePaper center="true">
-              <Body dangerouslySetInnerHTML={{ __html: this.state.lyrics }} />
+            {loading ?
+            <span>Loading lyrics...</span>
+            :
+            <Body dangerouslySetInnerHTML={{ __html: this.state.lyrics }} />
+            }
             </WhitePaper>
           </Grid>
 
           <Grid item md={6} xs={12}>
             <WhitePaper center="true">
-              <Body>Translated</Body>
+                {translatedLyrics ?
+                <Body dangerouslySetInnerHTML={{__html: this.state.translatedLyrics}} />
+                :
+              <Body>
+                <h2> Translate to</h2>
+                <Section width={`210`}>
+                  <Button onClick={() => this.translateText('es')} style={{ margin: '3px' }}>Spanish</Button>
+                  <Button onClick={() => this.translateText('en')} style={{ margin: '3px' }}>English</Button>
+                  <Button onClick={() => this.translateText('de')} style={{ margin: '3px' }}>German</Button>
+                  <Button onClick={() => this.translateText('fr')} style={{ margin: '3px' }}>French</Button>
+                  <Button onClick={() => this.translateText('it')} style={{ margin: '3px' }}>Italian</Button>
+                  <Button onClick={() => this.translateText('zh-CN')} style={{ margin: '3px' }}>Chinese</Button>
+                </Section>
+              </Body>
+                }
             </WhitePaper>
           </Grid>
         </Grid>
@@ -133,6 +260,11 @@ const Title = styled('h1')`
   letter-spacing: 2px;
   text-transform: uppercase;
   color: orange;
+`
+
+const Section = styled(`div`)`
+  width: ${props => (props.width ? `${props.width}px` : `auto`)};
+  margin: 10px auto;
 `
 
 const Body = styled('p')`
